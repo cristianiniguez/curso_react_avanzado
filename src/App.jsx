b4206847e0b6cc4aa6d218dc297d68dce72dda04
criniguez <felizpc1@gmail.com>
2021-01-02 13:11:11 -0400

    creando la interfaz: SVGR - de SVG a componente de ReactJS

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import { GlobalStyles } from './GlobalStyles';
+import { Logo } from './components/Logo';
 import { ListOfCategories } from './components/ListOfCategories';
 import { ListOfPhotoCards } from './components/ListOfPhotoCards';
 
 export const App = () => (
   <div>
     <GlobalStyles />
+    <Logo />
     <ListOfCategories />
     <ListOfPhotoCards />
   </div>
