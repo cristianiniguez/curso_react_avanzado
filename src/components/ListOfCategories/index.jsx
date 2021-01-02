15fab79940415a0fe8801856cf43e993e21228d8
criniguez <felizpc1@gmail.com>
2021-01-02 15:01:17 -0400

    hooks: useEffect - limpiando eventos

--- src/components/ListOfCategories/index.jsx
+++ src/components/ListOfCategories/index.jsx
@@ -4,6 +4,7 @@ import { List, Item } from './styles';
 
 export const ListOfCategories = () => {
   const [categories, setCategories] = useState([]);
+  const [showFixed, setShowFixed] = useState(false);
 
   useEffect(() => {
     fetch('https://petgram-server-criniguez.vercel.app/categories')
@@ -11,13 +12,31 @@ export const ListOfCategories = () => {
       .then((data) => setCategories(data));
   }, []);
 
-  return (
-    <List>
+  useEffect(() => {
+    const onScroll = (e) => {
+      const newShowFixed = window.scrollY > 200;
+      showFixed !== newShowFixed && setShowFixed(newShowFixed);
+    };
+
+    document.addEventListener('scroll', onScroll);
+
+    return () => document.removeEventListener('scroll', onScroll);
+  }, [showFixed]);
+
+  const renderList = (fixed) => (
+    <List className={fixed ? 'fixed' : ''}>
       {categories.map((category) => (
         <Item key={category.id}>
           <Category {...category} />
         </Item>
       ))}
     </List>
+  );
+
+  return (
+    <>
+      {renderList(false)}
+      {showFixed && renderList(true)}
+    </>
   );
 };
