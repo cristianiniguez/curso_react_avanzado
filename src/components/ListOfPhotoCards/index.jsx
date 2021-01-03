f6ed07a31fb1b87cd8db6bb31884489e6699c721
criniguez <felizpc1@gmail.com>
2021-01-03 15:01:01 -0400

    graphql y react apollo: inicializando react apollo client y primer HoC

--- src/components/ListOfPhotoCards/index.jsx
+++ src/components/ListOfPhotoCards/index.jsx
@@ -1,11 +1,30 @@
 import React from 'react';
+import { useQuery, gql } from '@apollo/client';
 import { PhotoCard } from '../PhotoCard';
 
+const withPhotos = gql`
+  query getPhotos {
+    photos {
+      id
+      categoryId
+      src
+      likes
+      userId
+      liked
+    }
+  }
+`;
+
 export const ListOfPhotoCards = () => {
+  const { loading, error, data } = useQuery(withPhotos);
+
+  if (loading) return <h2>Loading...</h2>;
+  if (error) return <h2>Internal Server Error</h2>;
+
   return (
     <ul>
-      {[1, 2, 3, 4, 5, 6, 7].map((id) => (
-        <PhotoCard key={id} id={id} />
+      {data.photos.map((photo) => (
+        <PhotoCard key={photo.id} {...photo} />
       ))}
     </ul>
   );
