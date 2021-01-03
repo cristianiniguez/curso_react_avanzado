f756e72ca0144ea5af27e1bbc37198170f2c4dea
criniguez <felizpc1@gmail.com>
2021-01-03 16:52:05 -0400

    graphql y react apollo: usando las mutaciones con los likes

--- src/components/PhotoCard/index.jsx
+++ src/components/PhotoCard/index.jsx
@@ -1,20 +1,36 @@
 import React from 'react';
-import { MdFavoriteBorder, MdFavorite } from 'react-icons/md';
+import { gql, useMutation } from '@apollo/client';
+
+import { FavButton } from '../Favbutton';
 
 import { useLocalStorage } from '../../hooks/useLocalStorage';
 import { useNearScreen } from '../../hooks/useNearScreen';
-import { Article, ImgWrapper, Img, Button } from './styles';
+import { Article, ImgWrapper, Img } from './styles';
 
 const DEFAULT_IMAGE =
   'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60';
+
+const LIKE_PHOTO = gql`
+  mutation likeAnonymousPhoto($input: LikePhoto!) {
+    likeAnonymousPhoto(input: $input) {
+      id
+      liked
+      likes
+    }
+  }
+`;
 
 export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
   const key = `like-${id}`;
 
   const [liked, setLiked] = useLocalStorage(key, false);
   const [shown, element] = useNearScreen();
+  const [toggleLike] = useMutation(LIKE_PHOTO);
 
-  const Icon = liked ? MdFavorite : MdFavoriteBorder;
+  const handleFavClick = () => {
+    !liked && toggleLike({ variables: { input: { id } } });
+    setLiked(!liked);
+  };
 
   return (
     <Article ref={element}>
@@ -25,10 +41,7 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
               <Img src={src} alt={''} />
             </ImgWrapper>
           </a>
-          <Button onClick={() => setLiked(!liked)}>
-            <Icon size='32px' />
-            {likes} likes!
-          </Button>
+          <FavButton liked={liked} likes={likes} onClick={handleFavClick} />
         </>
       )}
     </Article>
