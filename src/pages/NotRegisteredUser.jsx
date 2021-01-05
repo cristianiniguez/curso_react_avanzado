7ec05caf7cca3676ba624091cf5bb690a6bc4c6b
criniguez <felizpc1@gmail.com>
2021-01-05 12:08:26 -0400

    gestion del usuario: mutaciones para el registro

--- src/pages/NotRegisteredUser.jsx
+++ src/pages/NotRegisteredUser.jsx
@@ -1,16 +1,31 @@
 import React from 'react';
 import Context from '../Context';
 import { UserForm } from '../components/UserForm';
+import { gql, useMutation } from '@apollo/client';
 
-export const NotRegisteredUser = () => (
-  <Context.Consumer>
-    {({ activateAuth }) => {
-      return (
-        <>
-          <UserForm title='Registrarse' onSubmit={activateAuth} />
-          <UserForm title='Iniciar sesión' onSubmit={activateAuth} />
-        </>
-      );
-    }}
-  </Context.Consumer>
-);
+const REGISTER = gql`
+  mutation signup($input: UserCredentials!) {
+    signup(input: $input)
+  }
+`;
+
+export const NotRegisteredUser = () => {
+  const [register] = useMutation(REGISTER);
+
+  return (
+    <Context.Consumer>
+      {({ activateAuth }) => {
+        const onSubmit = ({ email, password }) => {
+          register({ variables: { input: { email, password } } }).then(activateAuth);
+        };
+
+        return (
+          <>
+            <UserForm title='Registrarse' onSubmit={onSubmit} />
+            <UserForm title='Iniciar sesión' onSubmit={activateAuth} />
+          </>
+        );
+      }}
+    </Context.Consumer>
+  );
+};
